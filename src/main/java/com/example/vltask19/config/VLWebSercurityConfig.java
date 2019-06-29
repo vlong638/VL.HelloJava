cf7f942f5b08a347b3f044f9d7a00758a840df7f
夏锦慧 <jinhui.xia@xiaobao100.com>
2019-06-29 19:09:10 +0800

    1.BCryptPasswordEncoder密码加密

--- src/main/java/com/example/vltask19/config/VLWebSercurityConfig.java
+++ src/main/java/com/example/vltask19/config/VLWebSercurityConfig.java
@@ -6,14 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class VLWebSercurityConfig extends WebSecurityConfigurerAdapter {
+
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
     }
 
     @Override
@@ -32,9 +33,15 @@ public class VLWebSercurityConfig extends WebSecurityConfigurerAdapter {
                 .password("123456")
                 .roles(AuthRoles.UserManager.toString())
                 .and()
-                .withUser("role")
+                .withUser("role1")
                 .password("123456")
-                .roles(AuthRoles.RoleManager.toString());
+                .roles(AuthRoles.RoleManager.toString())
+                /*加密*/
+                .and()
+                .withUser("role2")
+                .password(new BCryptPasswordEncoder(10).encode("456789"))
+                .roles(AuthRoles.RoleManager.toString())
+        ;
         /*TODO 改为基于数据库的登录及授权*/
         /*auth.jdbcAuthentication();*/
     }
@@ -47,11 +54,11 @@ public class VLWebSercurityConfig extends WebSecurityConfigurerAdapter {
                 .hasRole("ADMIN")
                 /*基于任一角色控制*/
                 .antMatchers("/user/**")
-                .access("hasAnyRole('"+AuthRoles.Admin.toString()+"','"+AuthRoles.UserManager.toString()+"')")
+                .access("hasAnyRole('" + AuthRoles.Admin.toString() + "','" + AuthRoles.UserManager.toString() + "')")
                 .antMatchers("/authority/**")
-                .access("hasAnyRole('"+AuthRoles.Admin.toString()+"','"+AuthRoles.AuthorityManager.toString()+"')")
+                .access("hasAnyRole('" + AuthRoles.Admin.toString() + "','" + AuthRoles.AuthorityManager.toString() + "')")
                 .antMatchers("/role/**")
-                .access("hasAnyRole('"+AuthRoles.Admin.toString()+"','"+AuthRoles.RoleManager.toString()+"')")
+                .access("hasAnyRole('" + AuthRoles.Admin.toString() + "','" + AuthRoles.RoleManager.toString() + "')")
                 /*除以上权限控制以外,仅需登录后即可访问*/
                 .anyRequest()
                 .authenticated()
@@ -66,6 +73,7 @@ public class VLWebSercurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable()
+        /*TODO json化交互*/
         ;
     }
 }
